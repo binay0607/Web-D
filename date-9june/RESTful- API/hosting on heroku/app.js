eb7471676a53490b6f5f8dd3b5c848eb6009fbfe
Binay Prasad <binay0607@gmail.com>
2022-07-09 02:53:00 +0530

    hosting on heroku

--- date-9june/RESTful- API/hosting on heroku/app.js
+++ date-9june/RESTful- API/hosting on heroku/app.js
@@ -122,6 +122,6 @@ app.route("/articles/:tag")
 
 
 
-app.listen(3000, function(){
-    console.log("server running on port 3000");
+app.listen(process.env.PORT, function(){
+    console.log("server running");
 });
